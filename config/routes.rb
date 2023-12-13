5212177093c5bc6e245cc7124af022ae639d3492
stino-x <107426247+stino-x@users.noreply.github.com>
2023-12-13 20:53:00 +0100

    Add user permissions for managing inventory and food

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'public_recipes/index'
+  # get 'public_recipes/index'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -9,7 +9,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  get 'shopping_list/recipe_id/:recipe_id/inventory_id/:inventory_id', to: 'shopping_lists#shopping_list', as: 'shopping_list'
 
+  resources :inventories, only: [:index, :new, :show, :create, :update, :destroy] do
+    resources :inventory_foods, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+  end
   resources :recipes do
     resources :recipe_foods
    end
