815dd0d3e958ed735bccbb34226609ba74580027
GhausAliKhan <ghauskhan1616@gmail.com>
2023-12-16 06:26:14 +0500

    Fix Linters

--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -1,12 +1,11 @@
 module RecipesHelper
-    def recipe_total_amount(recipe)
-      total_amount = 0
-  
-      recipe.recipe_foods.each do |recipe_food|
-        total_amount += recipe_food.quantity * recipe_food.food.price
-      end
-  
-      total_amount
+  def recipe_total_amount(recipe)
+    total_amount = 0
+
+    recipe.recipe_foods.each do |recipe_food|
+      total_amount += recipe_food.quantity * recipe_food.food.price
     end
+
+    total_amount
   end
-  
+end
